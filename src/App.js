1f1554e0f7f2fd8c54d4c1865886a92664692d3b
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-06-11 13:37:43 +0200

    changed task content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
-  { id: 2, content: "zjeść obiad", done: true },
+  { id: 2, content: "zjeść kolacje", done: true },
 ];
 
 const hideDoneTasks = false;
