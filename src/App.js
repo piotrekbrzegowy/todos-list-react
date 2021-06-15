22e595fb6937dec7cecd7c560154b32c1aa5324d
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-06-15 21:45:28 +0200

    added checking that the content is not empty

--- src/App.js
+++ src/App.js
@@ -39,14 +39,18 @@ function App() {
   };
 
   const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
+    if (content === ("")) {
+      return null;
+    };
+
+      setTasks(tasks => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        },
+      ]);
   };
 
   return (
